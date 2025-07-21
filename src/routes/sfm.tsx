7154f58395286a48f72a44af26f1f79c31804933
Brenley Dueck <brenleydueck@gmail.com>
2025-07-21 17:23:26 -0500

    Use count instead of todos

--- src/routes/sfm.tsx
+++ src/routes/sfm.tsx
@@ -4,38 +4,34 @@ import { createFileRoute } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 import { revalidate } from "../lib/sfm";
 
-
-export const todosQueryOptions = queryOptions({
-    queryKey: ['todos'],
-    queryFn: () => getTodos(),
+export const countQueryOptions = queryOptions({
+    queryKey: ['count'],
+    queryFn: () => getCount(),
 });
 
-const updateTodo = createServerFn({ method: 'POST' }).handler(() => {
-    const todos = JSON.parse(fs.readFileSync("data.json", "utf8"));
-    todos[0].title = `New Todo ` + Math.random();
+const updateCount = createServerFn({ method: 'POST' }).handler(() => {
+    const count = JSON.parse(fs.readFileSync("count.txt", "utf8"));
 
-    fs.writeFileSync("data.json", JSON.stringify(todos));
+    fs.writeFileSync("count.txt", String(parseInt(count) + 1));
 
-    return revalidate([todosQueryOptions as any]);
+    return revalidate([countQueryOptions as any]);
 })
 
-
-export const getTodos = createServerFn().handler(async () => {
-    return JSON.parse(fs.readFileSync("data.json", "utf8"));
+export const getCount = createServerFn().handler(async () => {
+    return JSON.parse(fs.readFileSync("count.txt", "utf8"));
 })
-
 
 export const Route = createFileRoute('/sfm')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
-    const { data: todos } = useSuspenseQuery(todosQueryOptions)
+    const { data: count } = useSuspenseQuery(countQueryOptions)
     const { mutate } = useMutation({
-        mutationFn: () => updateTodo()
+        mutationFn: () => updateCount()
     })
-    return <div>
-        {JSON.stringify(todos)}
-        <button onClick={() => mutate()}>Update Todo</button>
+    return <div className="text-center">
+        <p>{count}</p>
+        <button onClick={() => mutate()}>Update Count</button>
     </div>
 }
